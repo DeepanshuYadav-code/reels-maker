b449f8cb6ae6cdd443481feab97c37db25994b58
Your Name <yadavdeepanshu000@gmail.com>
2025-01-31 23:54:22 +0530

    Initial Commit

---
+++ lib/db.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI){
+	throw new Error("Please define mongodb uri in env file")
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+	cached = global.mongoose = {conn: null, promise: null};
+}
+
+export async = // 18 min
